cb4180b78ceee277c12f2f625c710626a9e0cf97
@magdiellinares <magdiellinares@gmail.com>
2022-09-22 18:50:36 -0400

    Class 3. Spring MvCConfig

--- src/main/java/com/mitocode/controller/PatientController.java
+++ src/main/java/com/mitocode/controller/PatientController.java
@@ -22,6 +22,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+//@RequestMapping(value = {"/patients", "patients2"})
 @RequestMapping("/patients")
 public class PatientController {
 
